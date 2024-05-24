87007612cf653bedd154cc49d6a16335f235fcc1
Rasim <rasim.mehtijev@insly.com>
2024-05-24 18:12:16 +0300

    for dima

--- src/CoinInfo.jsx
+++ src/CoinInfo.jsx
@@ -1,24 +1,30 @@
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Chart from "./Chart";
 
 function CoinInfo({ coinData }) {
   return (
-    <Row>
-      <Col>
-        <Row>
-          <Col>Logo</Col>
-          <Col>{coinData.name}</Col>
-        </Row>
-      </Col>
-      <Col>
-        <div>High 700000</div>
-        <div>Low 670000</div>
-      </Col>
-      <Col>
-        <div>Avarage 690000</div>
-        <div>Change -3%</div>
-      </Col>
-    </Row>
+    <>
+      <Row>
+        <Col>
+          <Row>
+            <Col>Logo</Col>
+            <Col>{coinData.name}</Col>
+          </Row>
+        </Col>
+        <Col>
+          <div>High 700000</div>
+          <div>Low 670000</div>
+        </Col>
+        <Col>
+          <div>Avarage 690000</div>
+          <div>Change -3%</div>
+        </Col>
+      </Row>
+      <Row>
+        <Chart />
+      </Row>
+    </>
   );
 }
 
